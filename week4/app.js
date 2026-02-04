463a62a2e5b5611e768029a6777b30715dde620d
jmarcial2001 <jmarcial@hawaii.edu>
2026-02-03 20:46:25 -1000

    Remove old code for HTTP server
    Removed old HTTP server code from app.js.

--- week4/app.js
+++ week4/app.js
@@ -59,31 +59,3 @@ console.log("Completed the files.");
 
 
 
-
-
-
-
-/**
-OLD CODE BELOW
-
-
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-
-  const fs = require("fs");
-  fs.appendFileSync("file2.txt", " More Hello World!");
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
- */
-
