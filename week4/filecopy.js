db100a5f2549c68dbefdbdda799e62c2d8102275
jmarcial2001 <jmarcial@hawaii.edu>
2026-02-07 05:00:49 -1000

    Removed files and added a new app.js file in the hello directory. Also modified some text files in the hero-hw4 output directory.

--- week4/filecopy.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-fs.copyFileSync("file1.txt", "file51.txt");
-fs.writeFileSync("file5.txt", "This is a new text - ");
-fs.appendFileSync("file5.txt", "Hello World");
---
+++ week4/hello/app.js
@@ -0,0 +1,4 @@
+// Jonas Marcial
+// Feb 2026
+// ICS 385 Week 4 - Hello World (Node)
+console.log("Aloha from Node.js!");
